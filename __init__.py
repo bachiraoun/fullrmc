df447070991c862420f3375d52656b9cd9a6afc3
Bach <bachir.aoun@e-aoun.com>
2015-02-03 14:26:51 -0600

    remove code from docs

--- __init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Reverse Monte Carlo (RMC) is probably best known for its applications in condensed matter 
-physics and solid state chemistry.\n
-fullrmc is a RMC modelling package to solve an inverse problem whereby an atomic/molecular 
-model is adjusted until its atoms positions have the greatest consistency with a set of experimental data.\n
-"""
-__version__ = 1.0

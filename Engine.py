generating random translation and rotation bug fixed

Engine.py
@@ -391,19 +391,24 @@ class Engine(object):
             self.__groups = []
             for g in groups:
                 # check group type
-                assert isinstance(g, np.ndarray), log.LocalLogger("fullrmc").logger.error("each group in groups must be a numpy.ndarray")
-                # check group dimension
-                assert len(g.shape) == 1, log.LocalLogger("fullrmc").logger.error("each group must be a numpy.ndarray of dimension 1")
-                if len(g)==0:
-                    continue
-                # check type
-                assert g.dtype.type is INT_TYPE, log.LocalLogger("fullrmc").logger.error("each group in groups must be of type numpy.int32")
-                # sort and check limits
-                g = sorted(set(g))
-                assert g[0]>=0, log.LocalLogger("fullrmc").logger.error("group index must equal or bigger than 0")
-                assert g[-1]<len(self.__pdb), log.LocalLogger("fullrmc").logger.error("group index must be smaller than number of atoms in pdb")
+                if isinstance(g, Group):
+                    assert np.max(g.indexes)<len(self.__pdb), log.LocalLogger("fullrmc").logger.error("group index must be smaller than number of atoms in pdb")
+                    gr = g
+                else:
+                    assert isinstance(g, (np.ndarray)), log.LocalLogger("fullrmc").logger.error("each group in groups must be a numpy.ndarray or fullrmc Group instance")
+                    # check group dimension
+                    assert len(g.shape) == 1, log.LocalLogger("fullrmc").logger.error("each group must be a numpy.ndarray of dimension 1")
+                    if len(g)==0:
+                        continue
+                    # check type
+                    assert g.dtype.type is INT_TYPE, log.LocalLogger("fullrmc").logger.error("each group in groups must be of type numpy.int32")
+                    # sort and check limits
+                    g = sorted(set(g))
+                    assert g[0]>=0, log.LocalLogger("fullrmc").logger.error("group index must equal or bigger than 0")
+                    assert g[-1]<len(self.__pdb), log.LocalLogger("fullrmc").logger.error("group index must be smaller than number of atoms in pdb")
+                    gr = Group(indexes=g)
                 # append group
-                self.__groups.append( Group(indexes=g) )
+                self.__groups.append( gr )
         # broadcast to constraints
         self.__broadcaster.broadcast("update groups")
         

Generators/Rotations.py
@@ -76,7 +76,8 @@ class RotationGenerator(MoveGenerator):
         # get rotation axis
         n = 0
         while n<PRECISION:
-            rotationAxis = np.random.random(3)-np.random.random(3)
+            #rotationAxis = np.random.random(3)-np.random.random(3)
+            rotationAxis = 1-2*np.random.random(3)
             n = np.linalg.norm(rotationAxis)
         rotationAxis /= n
         # get rotation angle

Generators/Translations.py
@@ -68,7 +68,8 @@ class TranslationGenerator(MoveGenerator):
             #. argument (object): Any python object. Not used in this generator.
         """
         # generate random vector
-        vector = np.array(np.random.random(3)-np.random.random(3), dtype=FLOAT_TYPE)
+        #vector = np.array(np.random.random(3)-np.random.random(3), dtype=FLOAT_TYPE)
+        vector = np.array(1-2*np.random.random(3), dtype=FLOAT_TYPE)
         # normalize vector
         vector /= FLOAT_TYPE( np.linalg.norm(vector) )
         # amplify vector
@@ -342,7 +343,8 @@ class TranslationCenterDirectionGenerator(TranslationGenerator):
         # amplify vectorsToCenter
         if self.__randomize:
             length = vector.shape[0]
-            amplitude = self.amplitude * np.array(np.random.random(length)-np.random.random(length), dtype=FLOAT_TYPE)
+            #amplitude = self.amplitude * np.array(np.random.random(length)-np.random.random(length), dtype=FLOAT_TYPE)
+            amplitude = self.amplitude * np.array(1-2*np.random.random(length), dtype=FLOAT_TYPE)
             vector[:,0] *= amplitude
             vector[:,1] *= amplitude
             vector[:,2] *= amplitude